c6a582e100b482edc6b18aba36fcaeaeeca8e110
Telmo Salvado <48351064+TelmoSalvado@users.noreply.github.com>
2019-11-18 11:32:30 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestãoTarefasIPG.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
