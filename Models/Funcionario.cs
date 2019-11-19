943829d1696df5f987e085db8d5dae36df9ff0f5
Telmo Salvado <48351064+TelmoSalvado@users.noreply.github.com>
2019-11-19 14:42:47 +0000

    Criado Modelo Funcionário

---
+++ Models/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestãoTarefasIPG.Models
+{
+    public class Funcionario
+    {
+        public int IDFuncionario { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Number { get; set; }
+
+    }
+}
