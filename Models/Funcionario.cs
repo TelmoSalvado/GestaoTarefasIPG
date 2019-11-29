aa548dfecd9142c5ed3dd514915fe50e6f4de402
Telmo Salvado <1700712@sal.ipg.pt>
2019-11-29 18:11:57 +0000

    Alteração do Template (inacabado)

--- Models/Funcionario.cs
+++ Models/Funcionario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,16 @@ namespace GestãoTarefasIPG.Models
     public class Funcionario
     {
         public int IDFuncionario { get; set; }
-
+        [Required]
         public string Nome { get; set; }
 
-        public int Number { get; set; }
+        public int Numero { get; set; }
 
+        public int Idade { get; set; }
+
+        public string Funcao { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
