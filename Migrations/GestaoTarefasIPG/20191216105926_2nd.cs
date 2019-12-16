52351154d0fef752642336849aebcfde0dff7eb3
Telmo Salvado <48351064+TelmoSalvado@users.noreply.github.com>
2019-12-16 10:59:40 +0000

    Update Seed data

---
+++ Migrations/GestaoTarefasIPG/20191216105926_2nd.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestãoTarefasIPG.Migrations.GestaoTarefasIPG
+{
+    public partial class _2nd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -9,35 +9,25 @@ namespace GestãoTarefasIPG.Models
     {
         public static void Populate(GestaoTarefasIPGContext db)
         {
-            {
-                if (db.Funcionario.Any())
-                {
-                    return;
-                }
-                db.Funcionario.AddRange(
-                    new Funcionario { Nome = "Gonçalo Santos", Idade = 32, Email = "estouchumbado@gmail.com", Numero = 1700780, Funcao = "Secretario" },
-                    new Funcionario { Nome = "Leonel Rita", Idade = 22, Email = "leonel@gmail.com", Numero = 1705250, Funcao = "Vice Diretor" },
-                    new Funcionario { Nome = "Telmo Salvado", Idade = 18, Email = "telmo@gmail.com", Numero = 17552780, Funcao = "Diretor" },
-                     new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" }
-                    );
-                db.SaveChanges();
-            }
-            if (db.Cargos.Any())
+            PopulateFuncionario(db);
+        }
+        private static void PopulateFuncionario(GestaoTarefasIPGContext db)
+        {
+            if (db.Funcionario.Any())
             {
                 return;
             }
-            db.Cargos.AddRange(
-                new Cargos { Nome = "Direção", Nível = 5 },
-                new Cargos { Nome = "Professores", Nível = 4 },
-                new Cargos { Nome = "Funcionarios", Nível = 2 }
-
-
+            db.Funcionario.AddRange(
+                new Funcionario { Nome = "Gonçalo Santos", Idade = 32, Email = "goncalo@gmail.com", Numero = 1700780, Funcao = "Secretario" },
+                new Funcionario { Nome = "Leonel Rita", Idade = 22, Email = "leonel@gmail.com", Numero = 1705250, Funcao = "Vice Diretor" },
+                new Funcionario { Nome = "Telmo Salvado", Idade = 18, Email = "telmo@gmail.com", Numero = 17552780, Funcao = "Diretor" },
+                 new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" }
                 );
             db.SaveChanges();
         }
 
 
     }
-    }
+}
 
 
