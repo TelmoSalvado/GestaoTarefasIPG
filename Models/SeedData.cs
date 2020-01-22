b9dd2f5f60273967d3e1f93f72168ee95ea7a1bf
Telmo Salvado <48351064+TelmoSalvado@users.noreply.github.com>
2020-01-22 22:13:17 +0000

    Last Commit. Atualização Seed Data, login tem erro ainda

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -12,7 +12,7 @@ namespace GestaoTarefasIPG.Models
         private const string FUNCIONARIO_ROLE = "func";
         public static void Populate(GestaoTarefasIPGContext db)
         {
-            // por ordem de utilização de chaves estrangeiras
+            
             PopulateCargos(db);
             PopulateProfessor(db);
             PopulateFuncionario(db);
@@ -76,7 +76,7 @@ namespace GestaoTarefasIPG.Models
         public static async Task CreateRolesAsync(RoleManager<IdentityRole> roleManager)
         {
 
-          /* if (!await roleManager.RoleExistsAsync(ADMIN_ROLE))
+          if (!await roleManager.RoleExistsAsync(ADMIN_ROLE))
             {
                 await roleManager.CreateAsync(new IdentityRole(ADMIN_ROLE));
             }
@@ -84,7 +84,7 @@ namespace GestaoTarefasIPG.Models
             if (!await roleManager.RoleExistsAsync(FUNCIONARIO_ROLE))
             {
                 await roleManager.CreateAsync(new IdentityRole(FUNCIONARIO_ROLE));
-            }*/
+            }
         }
        
         private static void PopulateCargos(GestaoTarefasIPGContext db)
@@ -94,9 +94,16 @@ namespace GestaoTarefasIPG.Models
                 return;
             }
             db.Cargos.AddRange(
-                new Cargos { Nome = "Diretor", Nível = 5 },
+                new Cargos { Nome = "Diretor da escola", Nível = 4},
                 new Cargos { Nome = "Tesoureiro", Nível = 3 },
-                new Cargos { Nome = "Limpeza", Nível = 1 }
+                new Cargos { Nome = "Limpeza", Nível = 1 },
+                new Cargos { Nome = "Vice-Diretor", Nível = 4 },
+                new Cargos { Nome = "Secretário", Nível = 2 },
+                new Cargos { Nome = "Contabilista", Nível = 3 },
+                new Cargos { Nome = "Rececionista", Nível = 1 },
+                new Cargos { Nome = "Técnico", Nível = 2 },
+                new Cargos { Nome = "Diretor do Ipg", Nível = 5},
+                new Cargos { Nome = "Professor", Nível = 3 }
                 );
 
             db.SaveChanges();
@@ -133,30 +140,16 @@ namespace GestaoTarefasIPG.Models
             db.Funcionario.AddRange(
                 new Funcionario { Nome = "Gonçalo Santos", Idade = 32, Email = "goncalo@gmail.com", Numero = 1700780, CargosId = 1 },
                 new Funcionario { Nome = "Leonel Rita", Idade = 22, Email = "leonel@gmail.com", Numero = 1705250, CargosId = 2 },
-                new Funcionario { Nome = "Telmo Salvado", Idade = 18, Email = "telmo@gmail.com", Numero = 17552780, CargosId = 3 }/*,
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" },
-                new Funcionario { Nome = "Teste1", Idade = 18, Email = "teste@1.com", Numero = 17552780, Funcao = "Tester" }*/
+                new Funcionario { Nome = "Telmo Salvado", Idade = 18, Email = "telmo@gmail.com", Numero = 17552780, CargosId = 3 },
+                new Funcionario { Nome = "Manuel Proença", Idade = 48, Email = "manu@gmail.com", Numero = 1755230, CargosId = 4 },
+                new Funcionario { Nome = "Manuela Tobias", Idade = 50, Email = "manuela@gmail.com", Numero = 1123230, CargosId = 5 },
+                new Funcionario { Nome = "Raquel Gomes", Idade = 20, Email = "raqui@gmail.com", Numero = 212580, CargosId = 6 },
+                new Funcionario { Nome = "Jéssica Trindade", Idade = 25, Email = "jessica@gmail.com", Numero = 1712312, CargosId = 7 },
+                new Funcionario { Nome = "Maria Antónia", Idade = 55, Email = "maria@gmail.com", Numero = 11231230, CargosId = 8 },
+                new Funcionario { Nome = "António Manuel", Idade = 35, Email = "to@gmail.com", Numero = 1755230, CargosId = 9 },
+                new Funcionario { Nome = "Joao Miguel", Idade = 48, Email = "joao@gmail.com", Numero = 1755230, CargosId = 10 }
+
+
                 );
 
             db.SaveChanges();
